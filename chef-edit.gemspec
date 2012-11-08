88824a8f136384904d16e69377c5834fd6d0455f
Bryan Berry <bryan.berry@gmail.com>
2012-11-08 14:22:07 +0100

    revert earlier commit that sets cookbook_name

--- chef-edit.gemspec
+++ chef-edit.gemspec
@@ -12,5 +12,5 @@ Gem::Specification.new do |gem|
   gem.test_files    = gem.files.grep(%r{^(test|spec|features)/})
   gem.name          = "chef-edit"
   gem.require_paths = ["lib"]
-  gem.version       = "0.0.3"
+  gem.version       = "0.0.4"
 end
--- lib/chef/edit.rb
+++ lib/chef/edit.rb
@@ -20,10 +20,9 @@ class Chef
     # block<Proc>:: Block with attributes to edit or create
     def edit(resource_id,  &block)
       begin
-        r = resources(resource_id)  
+        r = resources(resource_id)
         Chef::Log.info "Resource #{resource_id} found, now editing it"
         r.instance_exec(&block) if block
-        r.cookbook_name = cookbook_name
       rescue Chef::Exceptions::ResourceNotFound => e
         Chef::Log.info "Resource #{resource_id} not found, so edit fails"
         raise e
